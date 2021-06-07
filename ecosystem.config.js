0c192b5c37c7326bfaa50a086b9e70ec4429d92f
elinberg <elinberg@gmail.com>
2021-06-07 12:38:39 -0500

    build

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -17,7 +17,7 @@ module.exports = {
       repo : 'git@github.com:elinberg/signal-app.git',
       path : '/home/node/signal-app',
       'pre-deploy-local': '',
-      'post-deploy' : 'git pull && npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy' : 'git stash && git pull && npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import './fonts/DIN/DINRegular/DINRegular.ttf';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
 
-//serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
