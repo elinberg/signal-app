2d6fc50c1c65a5d79d0d7bb039760a15fe0d6eb8
elinberg <elinberg@gmail.com>
2021-06-07 11:53:32 -0500

    eco

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -131,7 +131,7 @@ const Login = () => {
                     <label>User Name: </label>
                     <input  type="text"
                             className="form-control"
-                            placeholder="username or email"
+                            placeholder="Username or Email"
                             autoComplete="username"
                             value={data.username}
                             onChange={onChangeUsername}
@@ -141,7 +141,7 @@ const Login = () => {
                     <label>Password: </label>
                     <input 
                             type="password" 
-                            placeholder="password"
+                            placeholder="Password"
                             className="form-control"
                             value={data.password}
                             autoComplete="current-password"
