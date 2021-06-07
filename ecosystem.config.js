4793df42f16c583eb1d359bc967acf0daed83e57
elinberg <elinberg@gmail.com>
2021-06-07 08:10:12 -0500

    ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -13,7 +13,7 @@ module.exports = {
       host : 'signal',
       ref  : 'origin/main',
       repo : 'signal:signal-app.git',
-      path : './',
+      path : '~/signal-app',
       'pre-deploy-local': '',
       'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
